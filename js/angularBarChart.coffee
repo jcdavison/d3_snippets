f3c869f624cc2c424e2cb60e4ef0524ddb23ed38
John Davison <johncdavison@gmail.com>
2013-09-13 15:35:38 -0700

    wip

--- js/angularBarChart.coffee
+++ js/angularBarChart.coffee
@@ -1,7 +1,7 @@
 app = angular.module("d3Charts", [])
 
 app.controller 'chartCtrl', ($scope) ->
-  $scope.widgets = "20,22,23"
+  $scope.widgets = ""
 
 app.directive 'csBar', () ->
   restrict: "A"
@@ -11,7 +11,7 @@ app.directive 'csBar', () ->
 
   link:  (scope, element, attrs) ->
     element.bind("click", () ->
-      console.log "fuck this"
+      console.log "click this"
     )
     data = scope.widgets.split(",")
     chart = d3.select("#barchart")
@@ -19,27 +19,12 @@ app.directive 'csBar', () ->
       .attr("width", "100%")
       .attr("height", "400px")
     y = 0
-    chart.selectAll("svg")
-      .selectAll("rect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("height", "30px")
-      .attr("rx", "5")
-      .attr("ry", "5")
-      .classed("rect", true)
-      .attr("width", (datum) ->
-        datum 
-      )
-      .attr("y", (datum) ->
-        y += 35 
-      )
+    x = 0
 
     scope.$watch 'widgets', (n, o) ->
-
       newData = n.split(",") 
       oldData = o.split(",")
-      if oldData.length != newData.length
+      if n && oldData.length != newData.length
         console.log "length !="
         chart.selectAll("svg")
           .selectAll("rect")
@@ -50,21 +35,14 @@ app.directive 'csBar', () ->
           .attr("rx", "5")
           .attr("ry", "5")
           .classed("rect", true)
-          .attr("width", (datum) ->
-            datum 
-          )
-          .attr("y", (datum) ->
-            y += 35 
-          )
-      if n && oldData.length == newData.length
+          .attr("width", (datum) -> datum )
+          .attr("y", (datum) -> y += 35 )
+
+      if oldData.length == newData.length
         console.log "length =="
-        data = n.split(",")
-        x = 0
         chart.selectAll("svg")
           .selectAll("rect")
-          .data(data)
+          .data(newData)
           .transition()
-          .attr("width", (datum) ->
-            datum * 5 + "px" )
-          .attr("fill", (datum) ->
-            "#3366" + x +=10  )
+          .attr("width", (datum) -> datum )
+          .attr("fill", (datum) -> "#3366" + x +=1)
--- js/angularBarChart.js
+++ js/angularBarChart.js
@@ -5,7 +5,7 @@
   app = angular.module("d3Charts", []);
 
   app.controller('chartCtrl', function($scope) {
-    return $scope.widgets = "20,22,23";
+    return $scope.widgets = "";
   });
 
   app.directive('csBar', function() {
@@ -16,24 +16,20 @@
       },
       template: '<div id="barchart">{{info}}</div>',
       link: function(scope, element, attrs) {
-        var chart, data, y;
+        var chart, data, x, y;
         element.bind("click", function() {
-          return console.log("fuck this");
+          return console.log("click this");
         });
         data = scope.widgets.split(",");
         chart = d3.select("#barchart");
         chart.append("svg").attr("width", "100%").attr("height", "400px");
         y = 0;
-        chart.selectAll("svg").selectAll("rect").data(data).enter().append("rect").attr("height", "30px").attr("rx", "5").attr("ry", "5").classed("rect", true).attr("width", function(datum) {
-          return datum;
-        }).attr("y", function(datum) {
-          return y += 35;
-        });
+        x = 0;
         return scope.$watch('widgets', function(n, o) {
-          var newData, oldData, x;
+          var newData, oldData;
           newData = n.split(",");
           oldData = o.split(",");
-          if (oldData.length !== newData.length) {
+          if (n && oldData.length !== newData.length) {
             console.log("length !=");
             chart.selectAll("svg").selectAll("rect").data(newData).enter().append("rect").attr("height", "30px").attr("rx", "5").attr("ry", "5").classed("rect", true).attr("width", function(datum) {
               return datum;
@@ -41,14 +37,12 @@
               return y += 35;
             });
           }
-          if (n && oldData.length === newData.length) {
+          if (oldData.length === newData.length) {
             console.log("length ==");
-            data = n.split(",");
-            x = 0;
-            return chart.selectAll("svg").selectAll("rect").data(data).transition().attr("width", function(datum) {
-              return datum * 5 + "px";
+            return chart.selectAll("svg").selectAll("rect").data(newData).transition().attr("width", function(datum) {
+              return datum;
             }).attr("fill", function(datum) {
-              return "#3366" + (x += 10);
+              return "#3366" + (x += 1);
             });
           }
         });
