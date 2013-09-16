4f26b73f0ff32122a9222271dbaccdf196ebe3b0
John Davison <johncdavison@gmail.com>
2013-09-16 15:26:06 -0700

    Updating Bar Chart works, see angular.html

--- js/angularBarChart.coffee
+++ js/angularBarChart.coffee
@@ -1,7 +1,21 @@
 app = angular.module("d3Charts", [])
 
 app.controller 'chartCtrl', ($scope) ->
-  $scope.widgets = ""
+  $scope.widgets = []
+  $scope.newVar = ""
+
+  $scope.addVar = () ->
+    $scope.widgets.push $scope.newVar
+    cleanWidgets()
+    console.log($scope.widgets)
+
+  $scope.removeVar = () ->
+    $scope.widgets.pop()
+    cleanWidgets()
+    console.log($scope.widgets)
+
+  cleanWidgets = () ->
+    $scope.widgets = _.compact($scope.widgets)
 
 app.directive 'csBar', () ->
   restrict: "A"
@@ -13,36 +27,51 @@ app.directive 'csBar', () ->
     element.bind("click", () ->
       console.log "click this"
     )
-    data = scope.widgets.split(",")
     chart = d3.select("#barchart")
     chart.append("svg")
       .attr("width", "100%")
       .attr("height", "400px")
     y = 0
-    x = 0
 
     scope.$watch 'widgets', (n, o) ->
-      newData = n.split(",") 
-      oldData = o.split(",")
-      if n && oldData.length != newData.length
-        console.log "length !="
-        chart.selectAll("svg")
-          .selectAll("rect")
-          .data(newData)
-          .enter()
-          .append("rect")
-          .attr("height", "30px")
-          .attr("rx", "5")
-          .attr("ry", "5")
-          .classed("rect", true)
-          .attr("width", (datum) -> datum )
-          .attr("y", (datum) -> y += 35 )
+      rectObjects = d3.selectAll("rect")[0].length
+      y = rectObjects * 35
 
-      if oldData.length == newData.length
-        console.log "length =="
-        chart.selectAll("svg")
-          .selectAll("rect")
-          .data(newData)
-          .transition()
-          .attr("width", (datum) -> datum )
-          .attr("fill", (datum) -> "#3366" + x +=1)
+      chart.selectAll("svg")
+        .selectAll("rect")
+        .data(n)
+        .enter()
+        .append("rect")
+        .attr("height", "30px")
+        .attr("rx", "5")
+        .attr("ry", "5")
+        .classed("rect", true)
+        .attr("width", (datum) -> datum )
+        .attr("y", (datum) -> y += 30 )
+        .attr("x", (datum) -> 0)
+
+
+      chart.selectAll("svg")
+        .selectAll("text")
+        .data(n)
+        .enter()
+        .append("text")
+        .text((d) -> Math.round(d) )
+        .attr("y", (datum) ->
+          y + 15 )
+        .attr("x", (datum) ->
+           Number(datum) + 5 )
+
+      chart.selectAll("svg")
+        .selectAll("rect")
+        .data(n)
+        .exit()
+        .transition()
+        .remove()
+
+      chart.selectAll("svg")
+        .selectAll("text")
+        .data(n)
+        .exit()
+        .transition()
+        .remove()
--- js/angularBarChart.js
+++ js/angularBarChart.js
@@ -5,7 +5,22 @@
   app = angular.module("d3Charts", []);
 
   app.controller('chartCtrl', function($scope) {
-    return $scope.widgets = "";
+    var cleanWidgets;
+    $scope.widgets = [];
+    $scope.newVar = "";
+    $scope.addVar = function() {
+      $scope.widgets.push($scope.newVar);
+      cleanWidgets();
+      return console.log($scope.widgets);
+    };
+    $scope.removeVar = function() {
+      $scope.widgets.pop();
+      cleanWidgets();
+      return console.log($scope.widgets);
+    };
+    return cleanWidgets = function() {
+      return $scope.widgets = _.compact($scope.widgets);
+    };
   });
 
   app.directive('csBar', function() {
@@ -16,35 +31,33 @@
       },
       template: '<div id="barchart">{{info}}</div>',
       link: function(scope, element, attrs) {
-        var chart, data, x, y;
+        var chart, y;
         element.bind("click", function() {
           return console.log("click this");
         });
-        data = scope.widgets.split(",");
         chart = d3.select("#barchart");
         chart.append("svg").attr("width", "100%").attr("height", "400px");
         y = 0;
-        x = 0;
         return scope.$watch('widgets', function(n, o) {
-          var newData, oldData;
-          newData = n.split(",");
-          oldData = o.split(",");
-          if (n && oldData.length !== newData.length) {
-            console.log("length !=");
-            chart.selectAll("svg").selectAll("rect").data(newData).enter().append("rect").attr("height", "30px").attr("rx", "5").attr("ry", "5").classed("rect", true).attr("width", function(datum) {
-              return datum;
-            }).attr("y", function(datum) {
-              return y += 35;
-            });
-          }
-          if (oldData.length === newData.length) {
-            console.log("length ==");
-            return chart.selectAll("svg").selectAll("rect").data(newData).transition().attr("width", function(datum) {
-              return datum;
-            }).attr("fill", function(datum) {
-              return "#3366" + (x += 1);
-            });
-          }
+          var rectObjects;
+          rectObjects = d3.selectAll("rect")[0].length;
+          y = rectObjects * 35;
+          chart.selectAll("svg").selectAll("rect").data(n).enter().append("rect").attr("height", "30px").attr("rx", "5").attr("ry", "5").classed("rect", true).attr("width", function(datum) {
+            return datum;
+          }).attr("y", function(datum) {
+            return y += 30;
+          }).attr("x", function(datum) {
+            return 0;
+          });
+          chart.selectAll("svg").selectAll("text").data(n).enter().append("text").text(function(d) {
+            return Math.round(d);
+          }).attr("y", function(datum) {
+            return y + 15;
+          }).attr("x", function(datum) {
+            return Number(datum) + 5;
+          });
+          chart.selectAll("svg").selectAll("rect").data(n).exit().transition().remove();
+          return chart.selectAll("svg").selectAll("text").data(n).exit().transition().remove();
         });
       }
     };
